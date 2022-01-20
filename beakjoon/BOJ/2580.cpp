45923b81b1320cb21c5ec98fc9b715fb2aa10aa5
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-01-20 23:35:35 +0900

    Create 2580.cpp
    일반 예제는 작동하지만, 작동하지 않는 경우도 있음, 수정해야함

---
+++ beakjoon/BOJ/2580.cpp
@@ -0,0 +1,102 @@
+/* 
+ * baekjoon 2580
+ * 
+ * 스도쿠
+ * 
+ * https://www.acmicpc.net/problem/2580
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int all[9][9];
+vector<pair<int,int>> v;
+
+bool *flag;
+int N;
+
+bool is_print = false;
+
+
+
+void print_sudoku(){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cout << all[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void solve(int x, int y){
+    int check[10]={0,};
+    for(int i=0;i<9;i++){
+        check[all[i][y]]=1;
+    }
+    for(int i=0;i<9;i++){
+        check[all[x][i]]=1;
+    }
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            check[all[(x/3)*3+i][(y/3)*3+j]]=1;
+        }
+    }
+
+    for(int i=1;i<=9;i++){
+        if(check[i]==0)
+            all[x][y] = i;
+    }
+
+    return;
+}
+
+
+
+
+void dfs(int depth){
+    if(is_print)    return;
+    if(depth == N){
+        print_sudoku();
+        is_print = true;
+        return;
+    }
+
+    for(int i=0;i<N;i++){
+        if(flag[i]) continue;
+        flag[i] = true;
+        solve(v[i].first,v[i].second);
+        dfs(depth+1);
+        flag[i] = false;
+        all[v[i].first][v[i].second] = 0;
+    }
+
+    return;
+}
+
+
+int main(){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cin >> all[i][j];
+            if(all[i][j]==0)
+                v.push_back({i,j});
+        }
+    }
+
+    N = v.size();
+    flag = new bool[N];
+
+    for(int i=0;i<N;i++){
+        flag[i] = false;
+    }
+
+    dfs(0);
+
+    return 0;
+}

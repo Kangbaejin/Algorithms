d181582649f662ac35d252393c3b227b5b044c6f
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-01-21 09:35:48 +0900

    Update 2580.cpp
    시간(402ms)

--- beakjoon/BOJ/2580.cpp
+++ beakjoon/BOJ/2580.cpp
@@ -15,11 +15,8 @@
 using namespace std;
 
 int all[9][9];
-vector<pair<int,int>> v;
 int check[10];
-bool *flag;
 int N;
-
 bool is_print = false;
 
 
@@ -31,79 +28,65 @@ void print_sudoku(){
         }
         cout << "\n";
     }
-}
-
-void solve(int x, int y){
-    for(int i=0;i<9;i++)    check[i] = 0;
-
-    for(int i=0;i<9;i++){
-        check[all[i][y]]=1;
-    }
-    for(int i=0;i<9;i++){
-        check[all[x][i]]=1;
-    }
-
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            check[all[(x/3)*3+i][(y/3)*3+j]]=1;
-        }
-    }
-
-    for(int i=1;i<=9;i++){
-        if(check[i]==0){
-            all[x][y] = i;
-            return;
-        }
-    }
 
     return;
 }
 
-
-
-
-void dfs(int depth){
-    if(is_print)    return;
-    if(depth > N)   return;
-    if(depth == N){
+void solve(int x, int y){
+    if(is_print) return;
+    else if(x==9 && y == 0){
         print_sudoku();
         is_print = true;
         return;
     }
+    else if(all[x][y]!=0){
+        if(y<8) solve(x,y+1);
+        else solve(x+1,0);
 
-    for(int i=0;i<N;i++){
-        if(flag[i]) continue;
-        flag[i] = true;
-        solve(v[i].first,v[i].second);
-        dfs(depth+1);
-        flag[i] = false;
-        all[v[i].first][v[i].second] = 0;
+        return;
     }
+
+    else{
+        for(int k=1;k<=9;k++){
+
+            for(int i=0;i<=9;i++)    check[i] = 0;
+
+            for(int i=0;i<9;i++){
+                check[all[i][y]]=1;
+            }
+            for(int i=0;i<9;i++){
+                check[all[x][i]]=1;
+            }
+
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    check[all[(x/3)*3+i][(y/3)*3+j]]=1;
+                }
+            }
+
+            if(check[k]==0){
+                all[x][y] = k;
+                if(y<8) solve(x,y+1);
+                else solve(x+1,0);
+                all[x][y] = 0;
+            }
+        }
+    }
+
 
     return;
 }
+
 
 
 int main(){
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
             cin >> all[i][j];
-            if(all[i][j]==0)
-                v.push_back({i,j});
         }
     }
 
-    cout << "\n";
-
-
-    N = v.size();
-    flag = new bool[N];
-
-    for(int i=0;i<N;i++){
-        flag[i] = false;
-    }
-
-    dfs(0);
+    solve(0,0);
 
     return 0;
 }

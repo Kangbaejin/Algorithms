9c041e71967a97d8232ab58c4c9eb61bc235eb29
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-01-21 17:58:51 +0900

    Update 1644.cpp
    부분합을 이용하는 방식으로 바꾸었는데도 시간초과가 발생했다.

--- beakjoon/BOJ/1644.cpp
+++ beakjoon/BOJ/1644.cpp
@@ -14,13 +14,14 @@ cout.tie(0);
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 vector<int> v;
+vector<int> sum_v;
 
 void eratosthenes(int n){
     if(n<=1)    return;
-
     bool* primes = new bool[n+1];
 
     for(int i=2; i<=n; i++)
@@ -34,7 +35,7 @@ void eratosthenes(int n){
 
     for(int i=2; i<=n; i++){
         if(primes[i]==true)
-        v.push_back(i);
+            v.push_back(i);
     }
 
     return;
@@ -55,16 +56,20 @@ int main(){
     int ans = 0;
     int temp = 0;
 
-    for(int sub = 1; sub<=size; sub++){
-        for(int i=0;i<=size-sub;i++){
-            temp = 0;
-            for(int j=0;j<sub;j++){
-                temp += v[i+j];
-            }
 
-            if(temp == N)
+    sum_v.push_back(0);
+    int s = 0;
+    for(int i=0;i<size;i++){
+        s += v[i];
+        sum_v.push_back(s);
+    }
+
+
+
+    for(int i=0;i<=size;i++){
+        for(int j=i+1;j<=size;j++){
+            if(sum_v[j]-sum_v[i]==N)
                 ans++;
-
         }
     }
 

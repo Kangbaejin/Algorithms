8668952cbac4fe132608ec3df45cc7061ad3ef9a
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-01-28 23:34:37 +0900

    Update 2667.cpp
    dfs 형태를 최근에 많이 보다 보니 간단히
    solve(n-1,depth+1);
    solve(n+1,depth+1);
    solve(n*2,depth+1);
    를 실행하면 구할 수 있을 것이라고 생각했는데, 그렇지 않았다.

--- beakjoon/BOJ/2667.cpp
+++ beakjoon/BOJ/2667.cpp
@@ -1,7 +1,7 @@
 /* 
  * baekjoon 2667
  * 
- * 단지 번호 붙이기
+ * 숨바꼭질
  * 
  * https://www.acmicpc.net/problem/2667
  * 
@@ -14,30 +14,24 @@
 
 using namespace std;
 
-int N,cnt;
-string mat[25];
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,-1,0,1};
+int ans;
+int x, y;
 
-vector<int> ans;
-
-bool visited[25][25]={false,};
-
-void dfs(int x, int y){
-    visited[x][y] = true;
-    cnt++;
-    for(int i=0;i<4;i++){
-        int newX = x + dx[i];
-        int newY = y + dy[i];
-
-        if((0<=newX && newX < N) && (0 <=newY && newY < N)){
-            if(mat[newX][newY]=='1' && !visited[newX][newY]){
-                dfs(newX, newY);
-            }
-        }
+void solve(int n, int depth){
+    cout << "depth : " << depth << " n : " << n << '\n';
+    if(n<0)  return;
+    if(depth>y-x)  return;
+    else if(n==y){
+        if(depth<ans) ans = depth;
+        return;
     }
-}
+    else{
+        solve(n-1,depth+1);
+        solve(n+1,depth+1);
+        solve(n*2,depth+1);
+    }
 
+}
 
 
 int main(){
@@ -45,24 +39,10 @@ int main(){
     cin.tie(0);
 
 
-    cin >> N;
-    for(int i=0;i<N;i++) cin >> mat[i];
-    
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            if(mat[i][j]=='1' && !visited[i][j]){
-                cnt = 0;
-                dfs(i,j);
-                ans.push_back(cnt);
-            }
-        }
-    }
-    sort(ans.begin(),ans.end());
+    cin >> x >> y;
+    ans = (y-x)>=0 ? y-x : x-y;
+    solve(x,0);
+    cout << ans;
 
-    cout << ans.size() << '\n';
-    for(int i=0;i<ans.size();i++){
-        cout << ans[i] << '\n';
-    }
-    
     return 0;
 }

6a4608811d4f3cf2a6c38a021b205ffd2d1c0dcc
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-03-29 08:59:11 +0900

    Update 20057.cpp

--- beakjoon/삼성SW역량테스트/20057.cpp
+++ beakjoon/삼성SW역량테스트/20057.cpp
@@ -27,6 +27,8 @@ using namespace std;
 
 int sand=0;
 int map[map_max][map_max];
+int dir_x[4] = {0,1,0,-1};
+int dir_y[4] = {-1,0,1,0};
 int N;
 
 void Input(){
@@ -38,38 +40,54 @@ void Input(){
     }
 }
 
-void tornado(int x, int y, int d_x, int d_y){
-    //1 1 2 2 7 7 5 10 나머지 % 순서로 구현
-    cout<<x << "," << y <<'\n';
-    int original = map[x][y];
-    int dir_x[10] = {-1,1,-2,2,0,-1,1,-1,1,0};
-    int dir_y[10] = {1,1,0,0,-2,0,0,-1,-1,-1};
+void Print(){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout << map[i][j] << " ";
+        }
+        cout <<"\n";
+    }
+
+    cout << "\n";
+}
+
+void tornado(int x, int y, int idx){
+    //1 1 2 2 7 7 5 10 10 나머지 % 순서로 구현
+    //cout<< x << "," << y <<'\n';
+    int xx = x + dir_x[idx];
+    int yy = y + dir_y[idx];
+    if(xx<0 || xx >=N || yy <0 || yy>=N) return;
+
+    int original = map[xx][yy];
+    int temp = original;
+    int tor_x[4][10] = {{-1,1,-2,2,0,-1,1,-1,1,0},
+                        {0,0,1,1,3,1,1,2,2,2},
+                        {-1,1,-2,2,0,-1,1,-1,1,0},
+                        {0,0,-1,-1,-3,-1,-1,-2,-2,-2}};
+    int tor_y[4][10] = {{0,0,-1,-1,-3,-1,-1,-2,-2,-2},
+                        {-1,1,-2,2,0,-1,1,-1,1,0},
+                        {0,0,1,1,3,1,1,2,2,2},
+                        {-1,1,-2,2,0,-1,1,-1,1,0}};
+                        
     int percent[10] = {1,1,2,2,5,7,7,10,10,55};
     for(int i=0;i<10;i++){
-        int newx = x+dir_x[i];
-        int newy = y+dir_y[i];
-        float f_value = map[x][y]*(percent[i]/100);
-        int value = int(f_value);
+        int newx = x+tor_x[idx][i];
+        int newy = y+tor_y[idx][i];
+        int value = (original*percent[i])/100;
         if(i==9) {
-            value = original;
+            value = temp;
             // cout<< "remain = " << value <<'\n';
         }
-        if(newx<0 || newx >=N || newy <0 || newy>=N) {
-            sand+=value;
-            original -= value;
-        }
-        else{
-            map[newx][newy]+=value;
-            original -= value;
-        }
+        if(newx<0 || newx >=N || newy <0 || newy>=N) sand+=value;
+        else map[newx][newy]+=value;
+        temp -= value;
     }
-    map[x][y]=0;
+    map[xx][yy]=0;
+    //Print();
     return;
 }
 
 void moving_point(){
-    int dir_x[4] = {0,1,0,-1};
-    int dir_y[4] = {-1,0,1,0};
     int distance = 1;
     //총 직선의 갯수는 2N-1 번   마지막 distance 는 3번 움직인다.
     //0,0 위치에 도달했으면 break 하는 방식 사용
@@ -80,11 +98,13 @@ void moving_point(){
     while(tests--){
         int inner_loop = distance;
         while(inner_loop--){
+            tornado(x,y,idx);
             x += dir_x[idx];
             y += dir_y[idx];
-            tornado(x,y, dir_x[idx], dir_y[idx]);
+            
             //cout << "(x,y) = " << "(" << x << ","  << y << ")\n";
             if(x==0 && y==0){
+                tornado(x,y,idx);
                 return;
             }
         }
@@ -93,9 +113,10 @@ void moving_point(){
 
         inner_loop = distance;
         while(inner_loop--){
+            tornado(x,y,idx);
             x += dir_x[idx];
             y += dir_y[idx];
-            tornado(x,y, dir_x[idx], dir_y[idx]);
+            
             //cout << "(x,y) = " << "(" << x << ","  << y << ")\n";
         }
         idx++;

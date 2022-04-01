6e60c653eba3fc254d8da187a099f3cb829deab0
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-04-01 12:02:42 +0900

    Update 23288.cpp

--- beakjoon/삼성SW역량테스트/23288.cpp
+++ beakjoon/삼성SW역량테스트/23288.cpp
@@ -6,10 +6,6 @@
  * https://www.acmicpc.net/problem/23288
  * 
  * 
- * 동서남북으로 연속으로 이동할 수 있는 칸의 수?
- * 
- * 
- * 예제 5번부터 오류가 있다
  * 
  */
 
@@ -40,6 +36,9 @@ void Input(){
     one_dice.x = 1;
     one_dice.y = 1;
     one_dice.dir = 1;
+    for(int i=1;i<=6;i++){
+        one_dice.dice_plane[i]=i;
+    }
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
             cin >> map[i][j];
@@ -49,15 +48,18 @@ void Input(){
 
 //연속된 칸수를 구하는 갯수
 //bfs 형태로 구현
-int cont_count(int x, int y){
+int cont_count(int xx, int yy){
     int visited[map_max][map_max]={false,};
-    int value = map[x][y];
-    visited[x][y] = true;
+    int value = map[xx][yy];
+    
     int count = 1;
 
     queue<pair<int, int>> q;
-    q.push({x,y});
+    q.push({xx,yy});
+    visited[xx][yy] = true;
+
     while(!q.empty()){
+
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
@@ -73,7 +75,6 @@ int cont_count(int x, int y){
         }
 
     }
-
     return value * count;
 }
 
@@ -81,71 +82,70 @@ int cont_count(int x, int y){
 
 
 int moving(dice &dice){
-    cout << "moving start on (x, y)" << dice.x << " " <<dice.y <<'\n';
     int newr = dice.x+DIR[dice.dir][0];
     int newc = dice.y+DIR[dice.dir][1];
 
-    if(newr<0 || newr>=N || newc<0 || newc>= M){
+    if(newr<1 || newr>N || newc<1 || newc> M){
         if(dice.dir%2 == 0){
             dice.dir--;
         }
         else{
             dice.dir++;
         }
-        return 0;
+        newr = dice.x+DIR[dice.dir][0];
+        newc = dice.y+DIR[dice.dir][1];
     }
-    else{
-        int count = cont_count(newr,newc);
-        //동 1   오른쪽
-        if(dice.dir == 1){
-            int temp = dice.dice_plane[1];
-            dice.dice_plane[1] = dice.dice_plane[4];
-            dice.dice_plane[4] = dice.dice_plane[6];
-            dice.dice_plane[6] = dice.dice_plane[3];
-            dice.dice_plane[3] = temp;
-        }
-        //서 2   왼쪽
-        else if(dice.dir == 2){
-            int temp = dice.dice_plane[1];
-            dice.dice_plane[1] = dice.dice_plane[3];
-            dice.dice_plane[3] = dice.dice_plane[6];
-            dice.dice_plane[6] = dice.dice_plane[4];
-            dice.dice_plane[4] = temp;
-        }
-        //북 3
-        else if(dice.dir == 3){
-            int temp = dice.dice_plane[1];
-            dice.dice_plane[1] = dice.dice_plane[5];
-            dice.dice_plane[5] = dice.dice_plane[6];
-            dice.dice_plane[6] = dice.dice_plane[2];
-            dice.dice_plane[2] = temp;  
-        }
-        //남 4
-        else if(dice.dir == 4){
-            int temp = dice.dice_plane[1];
-            dice.dice_plane[1] = dice.dice_plane[2];
-            dice.dice_plane[2] = dice.dice_plane[6];
-            dice.dice_plane[6] = dice.dice_plane[5];
-            dice.dice_plane[5] = temp;  
-        }
-        //A > B
-        if(map[newr][newc] > dice.dice_plane[6]){
-            if(dice.dir == 1) dice.dir = 4;
-            else if(dice.dir == 2)  dice.dir = 3;
-            else if(dice.dir == 3)  dice.dir = 1;
-            else if(dice.dir == 4)  dice.dir = 2;
-        }
-        //A < B
-        else if(map[newr][newc] < dice.dice_plane[6]){
-            if(dice.dir == 1) dice.dir = 3;
-            else if(dice.dir == 2)  dice.dir = 4;
-            else if(dice.dir == 3)  dice.dir = 2;
-            else if(dice.dir == 4)  dice.dir = 1;
-        }   
-        dice.x = newr;
-        dice.y = newc;
-        return count;
+    //동 1   오른쪽
+    if(dice.dir == 1){
+        int temp = dice.dice_plane[1];
+        dice.dice_plane[1] = dice.dice_plane[4];
+        dice.dice_plane[4] = dice.dice_plane[6];
+        dice.dice_plane[6] = dice.dice_plane[3];
+        dice.dice_plane[3] = temp;
     }
+    //서 2   왼쪽
+    else if(dice.dir == 2){
+        int temp = dice.dice_plane[1];
+        dice.dice_plane[1] = dice.dice_plane[3];
+        dice.dice_plane[3] = dice.dice_plane[6];
+        dice.dice_plane[6] = dice.dice_plane[4];
+        dice.dice_plane[4] = temp;
+    }
+    //북 3
+    else if(dice.dir == 3){
+        int temp = dice.dice_plane[1];
+        dice.dice_plane[1] = dice.dice_plane[5];
+        dice.dice_plane[5] = dice.dice_plane[6];
+        dice.dice_plane[6] = dice.dice_plane[2];
+        dice.dice_plane[2] = temp;  
+    }
+    //남 4
+    else if(dice.dir == 4){
+        int temp = dice.dice_plane[1];
+        dice.dice_plane[1] = dice.dice_plane[2];
+        dice.dice_plane[2] = dice.dice_plane[6];
+        dice.dice_plane[6] = dice.dice_plane[5];
+        dice.dice_plane[5] = temp;  
+    }
+    //A > B  시계방향
+    if(dice.dice_plane[6] > map[newr][newc]){
+        if(dice.dir == 1) dice.dir = 4;
+        else if(dice.dir == 2)  dice.dir = 3;
+        else if(dice.dir == 3)  dice.dir = 1;
+        else if(dice.dir == 4)  dice.dir = 2;
+    }
+    //A < B   반시계 방향
+    else if(dice.dice_plane[6] < map[newr][newc]){
+        if(dice.dir == 1) dice.dir = 3;
+        else if(dice.dir == 2)  dice.dir = 4;
+        else if(dice.dir == 3)  dice.dir = 2;
+        else if(dice.dir == 4)  dice.dir = 1;
+    }   
+    dice.x = newr;
+    dice.y = newc;
+    
+    int count = cont_count(newr,newc);
+    return count;
 }
 
 
@@ -159,7 +159,6 @@ void solve(){
 
     cout << ans;
 }
-
 
 
 int main(){

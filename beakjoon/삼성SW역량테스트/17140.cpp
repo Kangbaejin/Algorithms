31ab8cffaffd1872ca2fe5e61e9fd29e44a26f78
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-04-02 22:07:59 +0900

    Create 17140.cpp

---
+++ beakjoon/삼성SW역량테스트/17140.cpp
@@ -0,0 +1,24 @@
+/* 
+ * baekjoon 17140
+ *
+ * 이차원 배열과 연산
+ * 
+ * https://www.acmicpc.net/problem/17140
+ * 
+ * 
+ * 구현 계획
+ * 
+ * 정렬하면서 새로운 수 가 추가되므로 벡터로 구현하는게 좋을 것 같다.
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main(){
+    return 0;
+}

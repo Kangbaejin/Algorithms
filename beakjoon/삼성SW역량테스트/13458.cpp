0c02d830f5f0ae68f39a0d911e0844937fcd5314
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-04-09 23:12:01 +0900

    Create 13458.cpp

---
+++ beakjoon/삼성SW역량테스트/13458.cpp
@@ -0,0 +1,29 @@
+/* 
+ * baekjoon 13458
+ * 
+ * 시험 감독
+ * 
+ * https://www.acmicpc.net/problem/13458
+ * 
+ * N 시험장의 수
+ * A 각 시험장의 응시자의 수
+ * B 총 감독관의 감시 인원 수
+ * C 부 감독관의 감시 인원 수
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N,A,B,C;
+
+void Input(){
+
+}
+
+
+int main(){
+    return 0;
+}

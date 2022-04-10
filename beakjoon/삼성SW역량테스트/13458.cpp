1f0480ddd905650ab21d383e406cdc3c5fa96c82
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-04-10 22:50:30 +0900

    Update 13458.cpp

--- beakjoon/삼성SW역량테스트/13458.cpp
+++ beakjoon/삼성SW역량테스트/13458.cpp
@@ -15,15 +15,42 @@
 
 #include <iostream>
 #include <vector>
+#define MAX 1000001
 using namespace std;
 
-int N,A,B,C;
+int N,A[MAX],B,C;
 
 void Input(){
+    cin >> N;
+    for(int i=0;i<N;i++){
+        cin >> A[i];
+    }
+    cin >> B >> C;
+}
 
+void solve(){
+    long long ans=N;
+    for(int i=0;i<N;i++){
+        A[i]-=B;
+    }
+
+    for(int i=0;i<N;i++){
+        if(A[i] > 0){
+            int q = A[i]/C;
+            int r = A[i]%C;
+            if(r==0){
+                ans += q;
+            }
+            else ans+=(q+1);
+        }
+    }
+    cout << ans;
 }
 
 
 int main(){
+    Input();
+    solve();
+
     return 0;
 }

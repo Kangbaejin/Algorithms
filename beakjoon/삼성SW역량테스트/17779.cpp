119f6ec84bf084b6eda6a41b8bd16820d3833c02
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-07-05 10:55:00 +0900

    Update 17779.cpp
    제출번호 : 45525390
    메모리 : 2028KB
    시간 : 16ms

--- beakjoon/삼성SW역량테스트/17779.cpp
+++ beakjoon/삼성SW역량테스트/17779.cpp
@@ -19,7 +19,7 @@
  * 
  * 모든 x, y, d1, d2 의 경우를 포함해야한다.
  * 
- * 1. (x,y) 에서 (1 ≤ x < x+d1+d2 ≤ N) , (1 ≤ y-d1 < y < y+d2 ≤ N) 기준을 만족해야한다.
+ * 1. (x,y) 가 주어진 상황에서 각 꼭짓점은 map 을 벗어나면 안된다. (이 조건이 문제에서 준 조건)
  * 2. 위의 조건을 만족하는 기준점인 (x,y) 에서 가능한 d1,d2 를 모두 탐색해야한다.
  * 
  * d1, d2 를 모두 각각 1로 초기화한 후 while 문 두개를 이용하여  d1 d2 조건을 만족하는 탐색 진행
@@ -27,7 +27,7 @@
  */
 
 #include <iostream>
-#include <cstdio>
+#include <algorithm>
 #define max_rc 21
 
 using namespace std;
@@ -36,7 +36,7 @@ using namespace std;
 int map[max_rc][max_rc];
 int zone[max_rc][max_rc];
 int popular[5];
-int gap = -1;
+int ans = -1;
 int N;
 
 void Input(){
@@ -50,12 +50,27 @@ void Input(){
 }
 
 bool valid_check(int x, int y, int d1, int d2){
-    if((1<=x && x+d1+d1 <= N) && (1<=y-d1 && y+d2 <= N)) return true;
-    else return false;
+    int x0 = x, y0 = y;
+    int x1 = x+d1, y1 = y-d1;
+    int x2 = x+d1+d2, y2 = y-d1+d2;
+    int x3 = x+d2, y3 = y+d2;
+
+    if(x1 > N || y1 < 1) return false;
+    if(x2 > N || y2 > N) return false;
+    if(x3 > N || y3 > N) return false;
+    if(x3 > N || y3 < 1) return false;
+    else return true;
 }
 
 
 void zone_separation(int x, int y, int d1, int d2){
+    //각 꼭짓점 좌표 -> 반시계 방향으로 0, 1, 2, 3 순서
+    int x0 = x, y0 = y;
+    int x1 = x+d1, y1 = y-d1;
+    int x2 = x+d1+d2, y2 = y-d1+d2;
+    int x3 = x+d2, y3 = y+d2;
+
+
     //모든 선거구를 5로 초기화 하고 시작
     for(int i=1; i<=N;i++){
         for(int j=1; j<=N; j++){
@@ -63,48 +78,52 @@ void zone_separation(int x, int y, int d1, int d2){
         }
     }
 
+
     //1번 선거구 설정
-    for(int i=1; i<x+d1; i++){
-        for(int j=1; j<=y; j++){
+    int end = y0;
+    for(int i=1; i<x1; i++){
+        if(i >= x0) end--;
+        for(int j=1; j<=end; j++){
             zone[i][j] = 1;
         }
     }
 
     //2번 선거구 설정
-    for(int i=1; i<=x+d2; i++){
-        for(int j=y+1; j<=N; j++){
+    int start=y0+1;
+    for(int i=1; i<=x3; i++){
+        if(i > x0) start++;
+        for(int j=start; j<=N; j++){
             zone[i][j] = 2;
         }
     }
 
     //3번 선거구 설정
-    for(int i=x+d1; i<=N; i++){
-        for(int j=1; j<y-d1+d2; j++){
+    end = y1-1;
+    for(int i=x1; i<=N; i++){
+        for(int j=1; j<=end; j++){
             zone[i][j] = 3;
+        }
+        if(end < y2-1){
+            end++;
         }
     }
 
     //4번 선거구 설정
-    for(int i=x+d2+1; i<=N; i++){
-        for(int j=y-d1+d2; j<=N; j++){
+    start = y3;
+    for(int i=x3+1; i<=N; i++){
+        for(int j=start; j<=N; j++){
             zone[i][j] = 4;
+        }
+        if(start > y2){
+            start--;
         }
     }
 
-    //5번 선거구 설정
-    int x0 = x, y0 = y;
-    int x1 = x+d1, y1 = y-d1;
-    int x2 = x1+d2, y2 = y1+d2;
-    int x3 = x2-d1, y3 = y2+d1;
-
-
-    for(int i=x+d2+1; i<=N; i++){
-        for(int j=y-d1+d2; j<=N; j++){
-            zone[i][j] = 4;
-        }
-    }    
-    
-
+    // //꼭지점 표시
+    // zone[x0][y0] = 0;
+    // zone[x1][y1] = 0;
+    // zone[x2][y2] = 0;
+    // zone[x3][y3] = 0;
 
     return;
 }
@@ -122,6 +141,24 @@ void print_zone(){
 
 void function_pop(int x, int y, int d1, int d2){
     //calculate pop
+    int sum[6]={0,0,0,0,0,0};
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            sum[zone[i][j]] += map[i][j];
+        }
+    }
+
+    sort(sum, sum+6);
+    int gap = sum[5]- sum[1];
+
+    if(ans == -1){
+        ans = gap;
+        return;
+    }
+
+    else{
+        ans = min(gap, ans);
+    }
     
 }
 
@@ -133,9 +170,9 @@ void solve(){
                 for(int d2=1; d2<=N; d2++){
                     if(valid_check(x,y,d1,d2)){
                         zone_separation(x,y,d1,d2);
-                        print_zone();
-                        cout << "\n";
-
+                        // print_zone();
+                        // cout << "\n";
+                        function_pop(x,y,d1,d2);
                     }
                 }
             }
@@ -150,7 +187,7 @@ void solve(){
 int main(){
     Input();
     solve();
-    cout << gap;
+    cout << ans;
 
     return 0;
 }

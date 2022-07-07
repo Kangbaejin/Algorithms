d343068e43b1f9b4a86b89dbdb853d23c8a13470
kang <31719888+Kangbaejin@users.noreply.github.com>
2022-07-07 19:23:56 +0900

    Update 16235.cpp
    https://www.acmicpc.net/board/view/86193
    질문의 도움을 받아 해결하였다.
    
    기존의 내가 작성했던 코드는 sort를 모든 tree에 대해서 진행했기 때문에 시간 낭비가 있었고, 봄에서 각 tree들을 탐색할 때 가을에 번식할 나무를 미리 뽑아내는 것을 한번에 처리할 수 있었다.

--- beakjoon/삼성SW역량테스트/16235.cpp
+++ beakjoon/삼성SW역량테스트/16235.cpp
@@ -10,49 +10,18 @@
 
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <algorithm>
 
 using namespace std;
 
-struct tree {
-    int x;
-    int y;
-    int age;
-    bool live;
-};
-
-
-/*
- * 새로운 tree 를 입력하면서 나이의 오름차순으로 배열하도록 한다
- * priority queue 로 작성하려 했으나, queue 는 중간에서 죽은 나무를 제거할 수 없었다.
- * 만약 시간 부족이 발생하면 pq 로 작성하여, live을 이용하여 빼낸 뒤에 pop 하는 방식 생각...
- *
- */
-struct cmp {
-	bool operator()(tree n1, tree n2) {
-		if(n1.age < n2.age)
-			return true;
-        else if(n1.age == n2.age)
-            if(n1.x < n2.x){
-                return true;
-            }
-            else if(n1.x == n2.x){
-                if(n1.y < n2.y){
-                    return true;
-                }
-            }
-		return false;
-	}
-};
-
-
-vector<tree> trees;
-
-
+deque<int> trees[10][10];
+vector<pair<pair<int,int>, int>> dead, parent;
 
 int N,M,K;
 int map[10][10];
-int plus_map[10][10];
+int S2D2[10][10];
+
 void print_map(){
     for (int i=0; i<N; i++){
         for (int j=0; j<N; j++){
@@ -63,39 +32,21 @@ void print_map(){
 }
 
 
-void print_tree(tree t){
-    cout << "x : " << t.x << "\n";
-    cout << "y : " << t.y << "\n";
-    cout << "age : " << t.age << "\n";
-    cout << "live : " << t.live << "\n";
-}
-
-void make_tree(int x, int y, int age){
-    tree a_tree;
-    a_tree.x = x;
-    a_tree.y = y;
-    a_tree.age = age;
-    a_tree.live = true;
-    trees.push_back(a_tree);
-}
 
 
 void Input(){
     cin >> N >> M >> K;
     for (int i=0; i<N; i++){
         for (int j=0; j<N; j++){
-            cin >> plus_map[i][j];
+            cin >> S2D2[i][j];
             map[i][j] = 5;
         }
     }
-
+    int ix, iy, iz;
     for(int i=0; i<M; i++){
-        int ix, iy, iage;
-        cin >> ix >> iy >> iage;
-        make_tree(ix-1, iy-1, iage);
+        cin >> ix >> iy >> iz;
+        trees[ix-1][iy-1].push_back(iz);
     }
-
-
 }
 
 /*
@@ -106,15 +57,32 @@ void Input(){
  */
 
 void spring(){
-    sort(trees.begin(), trees.end(), cmp());
-    int size = trees.size();
-    for(int i=0; i<size; i++){
-        int x = trees[i].x;
-        int y = trees[i].y;
-        if(map[x][y] < trees[i].age)   trees[i].live = false;
-        else{
-            map[x][y] = map[x][y]-trees[i].age;
-            trees[i].age++;
+    dead.clear();
+    parent.clear();
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            if(trees[i][j].size()==0) continue;
+            //trees deque 안에는 나무의 나이들이 들어있는데 그중 작은 것 부터
+            //처리하기 위해 sort 를 진행한다.
+            sort(trees[i][j].begin(),trees[i][j].end());
+            int size = trees[i][j].size();
+            for(int k=0; k<size; k++){
+                int a_age = trees[i][j].front();
+                trees[i][j].pop_front();
+                if(map[i][j] >= a_age){
+                    map[i][j]-= a_age;
+                    trees[i][j].push_back(a_age+1);
+                    //살아 남는 나무들 중 번식 가능한 경우를 미리 체크하여
+                    //parent 에 저장 후 가을때 사용
+                    if((a_age+1)%5==0){
+                        parent.push_back({{i,j},a_age+1});
+                    }
+                }
+                else{
+                    dead.push_back({{i,j},a_age});
+                }
+            }
+
         }
     }
 }
@@ -125,24 +93,15 @@ void spring(){
  * 죽은 나무의 나이를 2로 나눈 값이 나무가 있던 칸의 양분이 된다.
  */
 
-bool isdead(tree t){
-    if(!t.live) return true;
-    else return false;
-}
-
 void summer(){
-    int size = trees.size();
+    int size = dead.size();
     for(int i=0; i<size; i++){
-        //나무가 살아있다면 넘어감
-        if (trees[i].live) continue;
-        //여기로 넘어왔다는 것은 죽은 나무라는 뜻
-        int x = trees[i].x;
-        int y = trees[i].y;
+        int x = dead[i].first.first;
+        int y = dead[i].first.second;
+        int age = dead[i].second;
 
-        map[x][y]+=trees[i].age/2;
+        map[x][y] += age/2;
     }
-
-    trees.erase(remove_if(trees.begin(),trees.end(), isdead), trees.end());
 }
 
 /*
@@ -154,18 +113,16 @@ int dir[8][2] = {{1,0},{-1,0},{0,1},{0,-1},
                 {1,1},{-1,-1},{1,-1},{-1,1}};
 
 void fall(){
-    int size = trees.size();
+    int size = parent.size();
     for(int i=0; i<size; i++){
-        //나무의 나이가 5의 배수가 아니라면 넘어간다.
-        if(trees[i].age%5!=0) continue;
-        //나무의 나이가 5의 배수라면 번식을 수행한다.
-        int x = trees[i].x;
-        int y = trees[i].y;
+        int x = parent[i].first.first;
+        int y = parent[i].first.second;
         for(int j=0; j<8; j++){
             int newX = x + dir[j][0];
             int newY = y + dir[j][1];
             if(newX<0 || newX >=N || newY<0 || newY >=N) continue;
-            make_tree(newX,newY,1);
+            //8방향 중 통과된 위치에 나이가 1인 나무 추가
+            trees[newX][newY].push_back(1);
         }
     }
 }
@@ -173,13 +130,13 @@ void fall(){
 /*
  * 겨울
  * S2D2 가 땅에 양분을 추가한다. 각 칸에 추가되는 것은 해당 칸의 양분이므로
- * 저장했던 plus_map 값을 더해준다.
+ * 저장했던 S2D2 값을 더해준다.
  */
 
 void winter(){
     for (int i=0; i<N; i++){
         for (int j=0; j<N; j++){
-            map[i][j] += plus_map[i][j];
+            map[i][j] += S2D2[i][j];
         }
     }
 }
@@ -195,16 +152,26 @@ void one_year(){
     winter();
 }
 
+void solve(){
+    while(K--){
+        one_year();
+    }
+
+    int ans=0;
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            ans+=trees[i][j].size();
+        }
+    }
+
+    cout << ans;
+}
+
 int main(){
     ios::sync_with_stdio(false);
 	cin.tie(0);
     cout.tie(0);
     Input();
-
-    while(K--){
-        one_year();
-    }
-
-    cout << trees.size();
-
+    solve();
+    return 0;
 }

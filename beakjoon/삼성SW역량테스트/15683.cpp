7f97009fb84bd878ba34b2f643582a04f7de918d
Kangbaejin <31719888+Kangbaejin@users.noreply.github.com>
2022-07-12 12:31:27 +0900

    Update 15683.cpp
    제출번호 45914806
    메모리 2024KB
    시간 4ms

--- beakjoon/삼성SW역량테스트/15683.cpp
+++ beakjoon/삼성SW역량테스트/15683.cpp
@@ -6,382 +6,151 @@
  * https://www.acmicpc.net/problem/15683
  * 
  * 
- * 
- * cctv 1
- * 4가지 경우
- * 
- * cctv 2
- * 2가지 경우
- * 
- * cctv 3
- * 4가지
- * 
- * cctv 4
- * 4가지
- * 
- * cctv 5
- * 1가지
- * 
- * 
- * cctv 종류별로 가짓수를 oper 벡터에 푸쉬해서 보관,
- * 
- * rotate_oper 함수를 통해
- * {4, 4, 2} 가짓수의 경우라면
- * {1, 1, 1} 부터 시작해서
- * {1, 1, 1} -> {2, 1, 1} -> {3, 1, 1} -> {4, 1, 1} -> {1, 2, 1} 식으로
- * 모든 경우의수를 탐색할 수 있게 구현했다.
- * 
- * 문제의 예제 테스트케이스를 모두 통과했고,
- * 반례도 많이 찾아서 실행 했을때 모두 맞게나왔지만
- * 제출하자마자 %도 오르지 않고 바로 틀렸습니다가 출력됐다.
- * 
  */
 
 
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <algorithm>
 #define m_max 8
 using namespace std;
 
+struct cctv{
+	int type, x, y;
+};
+
 int N,M;
 int map[m_max][m_max];
-int min_v=0;
-vector <int> opers;
-vector <int> copy_oper;
-//int ans_map[m_max][m_max];
-
-void cctv1(int copy_map[m_max][m_max], int x, int y, int c){
-    int dir_x[4]={0,0,1,-1};
-    int dir_y[4]={1,-1,0,0};
-
-    int newx = x;
-    int newy = y;
-
-    while(1){
-        newx += dir_x[c-1];
-        newy += dir_y[c-1];
-        if(!(newx>=0 && newx<N && newy>=0 && newy<M)) break;
-        if(copy_map[newx][newy]==6) break;
-        else if(copy_map[newx][newy] == 0){
-            copy_map[newx][newy] = -1;
-        }
-    }
-
-    return;
-}
-
-void cctv2(int copy_map[m_max][m_max], int x, int y, int c){
-    if(c==1){
-        int new1 = x;
-        while(1){
-            new1 += 1;
-            if(!(new1>=0 && new1<N)) break;
-            if(copy_map[new1][y]==6) break;
-            else if(copy_map[new1][y] == 0){
-                copy_map[new1][y] = -1;
-            }
-        }
-        int new2 = x;
-        while(1){
-            new2 += -1;
-            if(!(new2>=0 && new2<N)) break;
-            if(copy_map[new1][y]==6) break;
-            else if(copy_map[new1][y]==0){
-                copy_map[new1][y] = -1;
-            }
-            
-        }
-    }
-
-    if(c==2){
-        int new1 = y;
-        int new2 = y;
-        while(1){
-            new1 += 1;
-            if(!(new1>=0 && new1<M)) break;
-            if(copy_map[x][new1] == 6) break;
-            else if(copy_map[x][new1] == 0){
-                copy_map[x][new1] = -1;
-            }
-        }
-
-        while(1){
-            new2 += -1;
-            if(!(new2>=0 && new2<M)) break;
-            if(copy_map[x][new2] == 6) break;
-            else if(copy_map[x][new2] == 0){ 
-                copy_map[x][new2] = -1;
-            }
-        }
-    }
-    return;
-}
-
-void cctv3(int copy_map[m_max][m_max], int x, int y, int c){
-    int dir_new1[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
-    int dir_new2[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
-
-    int new1_x = x;
-    int new1_y = y;
-
-    int new2_x = x;
-    int new2_y = y;
-
-    while(1){
-        new1_x += dir_new1[c-1][0];
-        new1_y += dir_new1[c-1][1];
-        if(!(new1_x>=0 && new1_x<N && new1_y>=0 && new1_y<M)) break;
-        if(copy_map[new1_x][new1_y] == 6) break;
-        else if(copy_map[new1_x][new1_y]==0){
-            copy_map[new1_x][new1_y] = -1;
-        }
-    }
-    
-    while(1){
-        new2_x += dir_new2[c-1][0];
-        new2_y += dir_new2[c-1][1];
-        if(!(new2_x>=0 && new2_x<N && new2_y>=0 && new2_y<M)) break;
-        if(copy_map[new2_x][new2_y] == 6) break;
-        else if(copy_map[new2_x][new2_y] == 0){
-            copy_map[new2_x][new2_y] = -1;
-        }
-    }
-
-    return;
-}
-
-void cctv4(int copy_map[m_max][m_max], int x, int y, int c){
-    int dir_new1[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
-    int dir_new2[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
-    int dir_new3[4][2] = {{0,-1},{-1,0},{0,1},{1,0}};
-
-    int new1_x = x;
-    int new1_y = y;
-
-    int new2_x = x;
-    int new2_y = y;
-
-    int new3_x = x;
-    int new3_y = y;
-
-    while(1){
-        new1_x += dir_new1[c-1][0];
-        new1_y += dir_new1[c-1][1];
-        if(!(new1_x>=0 && new1_x<N && new1_y>=0 && new1_y<M)) break;
-        if(copy_map[new1_x][new1_y] == 6) break;
-        else if(copy_map[new1_x][new1_y]==0){
-            copy_map[new1_x][new1_y] = -1;
-        }
-    }
-
-    while(1){
-        new2_x += dir_new2[c-1][0];
-        new2_y += dir_new2[c-1][1];
-        if(!(new2_x>=0 && new2_x<N && new2_y>=0 && new2_y<M)) break;
-        if(copy_map[new2_x][new2_y] == 6) break;
-
-        else if(copy_map[new2_x][new2_y]==0){
-            copy_map[new2_x][new2_y] = -1;
-        }
-    }
-
-    while(1){
-        new3_x += dir_new3[c-1][0];
-        new3_y += dir_new3[c-1][1];
-        if(!(new3_x>=0 && new3_x<N && new3_y>=0 && new3_y<M)) break;
-        if(copy_map[new3_x][new3_y] == 6) break;
-        else if(copy_map[new3_x][new3_y]==0){
-            copy_map[new3_x][new3_y] = -1;
-        }
-    }
-
-    return;
-}
-
-void cctv5(int copy_map[m_max][m_max], int x, int y){
-    int new1_x = x;
-    int new1_y = y;
-
-    int new2_x = x;
-    int new2_y = y;
-
-    int new3_x = x;
-    int new3_y = y;
-
-    int new4_x = x;
-    int new4_y = y;
-
-    while(1){
-        new1_x += 1;
-        new1_y += 0;
-        if(!(new1_x>=0 && new1_x<N && new1_y>=0 && new1_y<M)) break;
-        if(copy_map[new1_x][new1_y] == 6) break;
-        else if(copy_map[new1_x][new1_y]==0){
-            copy_map[new1_x][new1_y] = -1;
-        }
-    }
-
-    while(1){
-        new2_x += -1;
-        new2_y += 0;
-        if(!(new2_x>=0 && new2_x<N && new2_y>=0 && new2_y<M)) break;
-        if(copy_map[new2_x][new2_y] == 6) break;
-        else if(copy_map[new2_x][new2_y]==0){
-            copy_map[new2_x][new2_y] = -1;
-        }
-    }
-
-    while(1){
-        new3_x += 0;
-        new3_y += 1;
-        if(!(new3_x>=0 && new3_x<N && new3_y>=0 && new3_y<M)) break;
-        if(copy_map[new3_x][new3_y] == 6) break;
-        else if(copy_map[new3_x][new3_y]==0){
-            copy_map[new3_x][new3_y] = -1;
-        }
-    }
-
-    while(1){
-        new4_x += 0;
-        new4_y += -1;
-        if(!(new4_x>=0 && new4_x<N && new4_y>=0 && new4_y<M)) break;
-        if(copy_map[new4_x][new4_y] == 6) break;
-        else if(copy_map[new4_x][new4_y]==0){
-            copy_map[new4_x][new4_y] = -1;
-        }
-    }
-
-    return;
-}
-
-int check(int map[m_max][m_max]){
-    int z_count = 0;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            if(map[i][j] == 0){
-                z_count++;
-            }
-        }
-    }
-    return z_count;
-}
-
-void print_arr(int map[m_max][m_max]){
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            cout << map[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    return;
-}
-
-
-void one_action(vector<int> opers){
-    int copy_map[m_max][m_max];
-    memcpy(copy_map, map, sizeof(int)*m_max*m_max);
-    int idx =0;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            if(copy_map[i][j] == 1){
-                cctv1(copy_map, i, j, opers[idx]);
-                idx++;
-            }
-            if(copy_map[i][j] == 2){
-                cctv2(copy_map, i, j, opers[idx]);
-                idx++;
-            }
-            if(copy_map[i][j] == 3){
-                cctv3(copy_map, i, j, opers[idx]);
-                idx++;
-            }
-            if(copy_map[i][j] == 4){
-                cctv4(copy_map, i, j, opers[idx]);
-                idx++;
-            }
-            if(copy_map[i][j] == 5){
-                cctv5(copy_map, i, j);
-                idx++;
-            }
-        }
-    }
-
-
-
-    int check_zero = check(copy_map);
-
-    // print_arr(copy_map);
-
-    // cout << "\nzero count : " <<check_zero <<'\n';
-
-    if(min_v > check_zero) {
-        //memcpy(ans_map, copy_map, sizeof(int)*m_max*m_max);
-        min_v = check_zero;
-    }
-}
-
-void rotate_oper(vector<int> dest, vector<int> start){
-    while(1){
-        one_action(start);
-        if(dest == start) break;
-        else{
-            start[0]++;
-            for(int i=0;i<start.size()-1;i++){
-                if(start[i]>dest[i]){
-                    start[i+1]++;
-                    start[i] = 1;
-                }
-            }
-        }
-    }
-}
-
-
-
-
-void solve(){
-    rotate_oper(opers,copy_oper);
-    return;
-}
-
+vector<cctv> cam_vector;
+int ans = -1;
 
 
 void Input(){
-    cin >> N >> M;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            cin >> map[i][j];
-            if(map[i][j]==0) min_v++;
-            if(map[i][j]==1){
-                opers.push_back(4);
-            }
-            if(map[i][j]==2){
-                opers.push_back(2);
-            }
-            if(map[i][j]==3){
-                opers.push_back(4);
-            }
-            if(map[i][j]==4){
-                opers.push_back(4);
-            }
-            if(map[i][j]==5){
-                opers.push_back(1);
-            }
-        }
-    }
-
-    for(int i=0;i<opers.size();i++){
-        copy_oper.push_back(1);
-    }
+	cin >> N >> M;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			cin >> map[i][j];
+			if(map[i][j]!=0 && map[i][j]!=6){
+				cam_vector.push_back({map[i][j],i,j});
+			}
+		}
+	}
 }
 
 
-int main(){
-    Input();
-    solve();
-    //print_arr(ans_map);
-    cout << min_v;
+void watch(int d, int x, int y){
+	//dfs 를 진행할 때 방향 조정을 위해 나머지 연산
+	d %= 4;
+	switch (d) {
+		//위
+		case 0:
+			for(int i=x-1; i>=0; i--){
+				if(map[i][y] == 6){
+					break;
+				}
+				else if(map[i][y] == 0){
+					map[i][y] = -1;
+				}
+			}
+			break;
+		//오른쪽
+		case 1:
+			for(int i=y+1; i<M; i++){
+				if(map[x][i] == 6){
+					break;
+				}
+				else if(map[x][i] == 0){
+					map[x][i] = -1;
+				}
+			}
+			break;
+		//아래
+		case 2:
+			for(int i=x+1; i<N; i++){
+				if(map[i][y] == 6){
+					break;
+				}
+				else if(map[i][y] == 0){
+					map[i][y] = -1;
+				}
+			}
+			break;
 
-    return 0;
+		//왼쪽 (case 3)
+		default:
+			for(int i=y-1; i>=0; i--){
+				if(map[x][i] == 6){
+					break;
+				}
+				else if(map[x][i] == 0){
+					map[x][i] = -1;
+				}
+			}
+			break;
+	}
+}
+
+
+//DFS 를 통해 각 cam 하나씩 접근한다.
+//Input에서 저장한 cam_vector를 이용한다.
+int cam_settings_num[6] = {0, 4, 2, 4, 4, 1};
+void DFS(int idx_cam){
+	if(idx_cam==cam_vector.size()){
+		int bs=0;
+		for(int i=0;i<N;i++){
+			for(int j=0;j<M;j++){
+				if(map[i][j]==0) bs++;
+			}
+		}
+		if(ans == -1) ans = bs;
+		else ans = min(bs, ans);
+		return;
+	}
+
+	int copy_map[m_max][m_max];
+
+	int type = cam_vector[idx_cam].type;
+	int x = cam_vector[idx_cam].x;
+	int y = cam_vector[idx_cam].y;
+	for(int i=0;i<cam_settings_num[type];i++){
+		//이전 상태 저장
+		memcpy(copy_map, map, sizeof(int)*m_max*m_max);
+		
+		switch (type) {
+			//각 캠 종류마다 다른 감시 시행
+			case 1:
+				watch(i,x,y);
+				break;
+			case 2:
+				watch(i,x,y);
+				watch(i+2,x,y);
+				break;
+			case 3:
+				watch(i,x,y);
+				watch(i+1,x,y);
+				break;
+			case 4:
+				watch(i,x,y);
+				watch(i+1,x,y);
+				watch(i+2,x,y);
+				break;
+			default:
+				watch(i,x,y);
+				watch(i+1,x,y);
+				watch(i+2,x,y);
+				watch(i+3,x,y);
+				break;
+		}
+		DFS(idx_cam+1);
+		//원래 상태로 복원
+		memcpy(map, copy_map, sizeof(int)*m_max*m_max);
+	}
+}
+
+int main(){
+	Input();
+	DFS(0);
+	cout << ans;
+	return 0;
 }

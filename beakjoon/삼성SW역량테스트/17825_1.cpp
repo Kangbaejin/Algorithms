746d9b9ee6b751919b47c3eb177bc472b3390d75
Kangbaejin <31719888+Kangbaejin@users.noreply.github.com>
2022-07-15 23:44:35 +0900

    Update 17825_1.cpp
    2차로 구현하려다 실패한 코드 보존

--- beakjoon/삼성SW역량테스트/17825_1.cpp
+++ beakjoon/삼성SW역량테스트/17825_1.cpp
@@ -5,25 +5,6 @@
  * 
  * https://www.acmicpc.net/problem/17825
  * 
- *
- * 구현 계획
- * 테두리를 도는 점수는 +2 씩 진행된다.
- * 
- * 이때 10에서 출발할때, 20에서 출발할때, 30에서 출발할때 경로가 달라진다.
- * 해당 3가지 경우에 대한 경로는 따로 배열로 저장하여 이용한다.
- * 
- * 
- * 시작점 0, 도착점 -1 로 설정
- * 
- * 
- * 말이 이동을 마치는 칸에 다른 말이 있으면 그 말은 고를 수 없다.
- * 단, 이동을 마치는 칸이 도착 칸이면 고를 수 있다.
- * 라는 조건에 맞도록 구현해야한다.
- * 
- * 
- * moving 함수를 통해 변경된 값을 return 받아서,
- * solve 를 통해 모든 경우를 탐색할때 이용해야 하는가?
- * 그 단계까지의 총합을 기록하며 백트래킹? 하려면 그래야 할 것 같다.
  * 
  */
 
@@ -33,135 +14,198 @@
 
 using namespace std;
 
-int ans=-1;
+//다른 길이 집어넣고 관리하기 위해서 vector 이용
+vector<int> way_10 = {10,13,16,19,25,30,35,40};
+vector<int> way_20 = {20,22,24,25,30,35,40};
+vector<int> way_30 = {30,28,27,26,25,30,35,40};
+vector<int> out_way;  //0, 2, 4, ... 40
 
-int way_10[] = {10,13,16,19,25,30,35,40};
-int way_20[] = {22,24,25,30,35,40};
-int way_30[] = {28,27,26,25,30,35,40};
-
+vector<vector<int>> ways;
 int dice[10];
 
+/* 
+ * 말의 종류(번호)
+ * 어느 경로에 위치하는지 저장 (0: out, 1: way_10, 2: way_20, 3: way_30)
+ * 경로위의 현재 위치
+ * 도착점 도달여부
+ * ex) way : 0, idx : 0 은 시작지점이다.
+ */
+struct stone{
+    int way=0, idx=0;
+    bool end=false;
+};
+stone stones[5];
+
+int ans=0;
+vector<int> horse;
+vector<int> ans_v;
 
 void Input(){
+    //주사위 입력 처리
     for(int i=0;i<10;i++)   cin >> dice[i];
+
+    //경로 전처리  
+    for(int i=0;i<21;i++)   out_way.push_back(i*2);
+    //ways[0], ways[1], ways[2], ways[3] 으로 접근가능
+    ways.push_back(out_way);
+    ways.push_back(way_10);
+    ways.push_back(way_20);
+    ways.push_back(way_30);
+
 }
 
-//수행 불가 -> -2,  이동을 마칠 위치가 도착점 -> -1, 그렇지 않으면 해당 위치 return
-int moving(int *a, int dice_number, int * stones){
-    //이미 도착 지점에 있는 경우 return
-    if(*a == -1) return -2;
-    //이미 안쪽 경로에 있는 경우 체크
-    int flag = 0;
-    int idx = 0;
-    int newa = *a;
-    for(int i=0;i<8;i++){
-        if(newa == way_10[i]){
-            flag=1;
-            idx = i;
-            break;
-        }
-    }
-    for(int i=0;i<6;i++){
-        if(newa == way_20[i]){
-            flag=2;
-            idx = i;
-            break;
-        }
-    }
-    for(int i=0;i<7;i++){
-        if(newa == way_30[i]){
-            flag=3;
-            idx = i;
-            break;
-        }
-    }
-    int newidx = idx + dice_number;
-    //이미 안쪽에 있는 경우
-    if(flag==1){
-        if(newidx >= 8) newa = -1;
-        else newa = way_10[newidx];
-    }
-    if(flag==2){
-        if(newidx >= 6) newa = -1;
-        else newa = way_20[newidx];
-    }
-    if(flag==3){
-        if(newidx >= 7) newa = -1;
-        else newa = way_30[newidx];
+//도착한 칸의 값 return
+int move(stone& s, int distance){
+    int cur_way = s.way;
+    int cur_idx = s.idx;
+    vector<int> cur_way_vector = ways[cur_way];
+    
+    //경로 벗어나는 경우 -> 도착에 도달하는 경우
+    int next_idx = cur_idx + distance;
+    if(next_idx >= cur_way_vector.size()) {
+        s.end = true;
+        return 0;
     }
 
-    //바깥 쪽에 있는 경우
-    if(flag == 0){
-        newa = newa + 2*dice_number;
-        if(newa > 40) newa = -1;
+    //경로 내부에 존재하는 경우
+    //cur_way_vector[next_idx]는 이동 후의 값을 의미한다.
+    int next_value = cur_way_vector[next_idx];
+
+    //이동을 마쳤을 때 값이 10, 20, 30 이면 새로운 경로로 조정해야한다.
+    //10, 20, 30 이 아닌 경우는 업데이트하고 종료
+    switch (next_value){
+        case 10:
+            s.way = 1;
+            s.idx = 0;
+            break;
+        case 20:
+            s.way = 2;
+            s.idx = 0;
+            break;
+        case 30:
+            s.way = 3;
+            s.idx = 0;
+            break;
+        default :
+            s.idx = next_idx;
+            break;
+    }
+    return next_value;
+}
+
+void print_stones(){
+    cout << "####print####\n";
+    for(int i=1;i<=4;i++){
+        cout << "stone[" << i << "]\n";
+        cout << "way : " << stones[i].way << " ";
+        cout << "idx : " << stones[i].idx << " ";
+        cout << "end : " << stones[i].end << "\n";
     }
 
-    //이동을 마치는 칸에 이미 말이 있는지 체크
-    int flag2 =0;
-    for(int i=0;i<4;i++){
-        if(stones[i]==newa){
-            //도착칸은 상관 없음
-            if(stones[i]!=-1){
-                flag2 = -1;
-                break;
-            }
-        }
+    for(int i=0;i<ans_v.size();i++){
+        cout << ans_v[i] << " ";
     }
-    //이동하는 칸에 이미 말이 없는 경우 위치 업데이트
-    if(flag2==0){
-        *a = newa;
-    }
-    //이미 말이 있어서 이동할 수 없는 경우
-    else{
-        newa = -2;
-    }
-    return newa;
+    cout <<"\n";
 }
 
 
-
-int init_stone[4] = {0,0,0,0};
-
-void solve(int *stones, int sum, int depth, vector<int> v){
+void DFS(int depth, int sum){
     if(depth == 10){
-        if(ans < sum) {
+        if(sum>ans) {
+            cout << ans << "\n";
             ans = sum;
-            for(int i=0;i<10;i++) cout << v[i] << ' ';
-            cout << '\n';
+            ans_v = horse;
+            print_stones();
         }
-        
         return;
     }
+    
+    
+    for(int i=1;i<=4;i++){
+        // if(stones[i].end) continue;
+        horse.push_back(i);
+        //이전 상태 저장
+        stone temp[5];
+        memcpy(temp, stones, sizeof(struct stone)*5);
 
-    // for(int i=0;i<4;i++){
-    //     cout << stones[i] << " ";
-    // }
-    // cout << '\n';
+        int cur;
+        cur = move(stones[i],dice[depth]);
 
-    int newstone[4];
-    for(int i=0;i<4;i++){
-        memcpy(newstone, stones, sizeof(int)*4);
-        int val = moving(&newstone[i],dice[depth],newstone);
-        if(val == -1) val = 0;
-        //이미 도착칸에 있는 말을 움직이려 했거나,
-        //이동을 마치는 칸에 다른 말이 있는 경우
-        if(val == -2){
-            continue;
+
+        //겹치는지 체크
+        bool overlab_flag=false;
+        for(int j=1;j<=4;j++){
+            if(i==j) continue;
+            int other_val = ways[stones[j].way][stones[j].idx];
+            if(cur != other_val) continue;
+            //같은 값들 넘어왔다.
+            //way 랑 idx 다르면 다른 것으로 분류한다.
+            if(stones[j].way==stones[i].way && stones[j].idx == stones[i].idx){
+                overlab_flag = true;
+                break;
+            }
+            //하지만 25, 30, 35, 40 부분은 way, idx 다르더라도 겹친다.
+            //번거롭지만 일일이 index 따져서 처리해준다.
+            //25 : way10 5  way20 4 way30 5
+            //30 : way10 6  way20 5 way30 6
+            //35 : way10 7  way20 6 way30 7
+            //40 : way10 8  way20 7 way30 8 out_way 20
+            else{
+                switch (cur){
+                    case 25:
+                        if(stones[j].way==1 && stones[j].idx==4) overlab_flag = true;
+                        if(stones[j].way==2 && stones[j].idx==3) overlab_flag = true;
+                        if(stones[j].way==3 && stones[j].idx==4) overlab_flag = true;
+                        break;
+                    case 30:
+                        if(stones[j].way==1 && stones[j].idx==5) overlab_flag = true;
+                        if(stones[j].way==2 && stones[j].idx==4) overlab_flag = true;
+                        if(stones[j].way==3 && stones[j].idx==5) overlab_flag = true;
+                        break;
+                    case 35:
+                        if(stones[j].way==1 && stones[j].idx==6) overlab_flag = true;
+                        if(stones[j].way==2 && stones[j].idx==5) overlab_flag = true;
+                        if(stones[j].way==3 && stones[j].idx==6) overlab_flag = true;
+                        break;
+                    case 40:
+                        if(stones[j].way==0 && stones[j].idx==20) overlab_flag = true;
+                        if(stones[j].way==1 && stones[j].idx==7) overlab_flag = true;
+                        if(stones[j].way==2 && stones[j].idx==6) overlab_flag = true;
+                        if(stones[j].way==3 && stones[j].idx==7) overlab_flag = true;
+                        break;
+                    default :
+                        break;
+                }
+                
+            }
         }
+
+        //겹친다면 원래대로 돌려놓고 다음 탐색
+        if(overlab_flag) {
+            memcpy(stones, temp, sizeof(struct stone)*5);
+            horse.pop_back();
+        }
+        //겹치지 않는다면 DFS 진행
         else{
-            vector<int> newv(v);
-            newv.push_back(i);
-            solve(newstone, sum+val, depth+1, newv);
+            DFS(depth+1, sum+cur);
+            memcpy(stones, temp, sizeof(struct stone)*5);
+            horse.pop_back();
         }
+        
     }
 
-    return;
 }
+
+void solve(){
+    DFS(0,0);
+    cout << ans;
+}
+
 
 int main(){
     Input();
-    vector<int> init_v;
-    solve(init_stone, 0, 0, init_v);
-    cout << '\n' << ans;
+    solve();
+
+
     return 0;
 }
